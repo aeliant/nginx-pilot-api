2c41ce1358bcffaa28b2813706830634a9ad1cba
Querdos <hamza.essayegh@protonmail.com>
2018-12-07 15:17:29 +0100

    Add first version of configuration file

---
+++ nginx_pilot_api/config.py
@@ -0,0 +1,41 @@
+# coding=utf-8
+import os
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    """ Base Configuration for all 3 environments. """
+
+    SECRET_KEY = os.getenv('SECRET_KEY', 'secret-to-change')
+    DEBUG = False
+
+
+class DevelopmentConfig(Config):
+    """ Development configuration. """
+
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    """ Testing configuration. """
+
+    DEBUG = True
+    TESTING = True
+
+
+class ProductionConfig(Config):
+    """ Production configurationt. """
+
+    DEBUG = False
+
+
+config_by_name = dict(
+    dev=DevelopmentConfig,
+    tests=TestingConfig,
+    prod=ProductionConfig
+)
+
+
+key = Config.SECRET_KEY
