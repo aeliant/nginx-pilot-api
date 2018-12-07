76008ea659ff4e2ae50fa035eb7d960a3a655aa6
Querdos <hamza.essayegh@protonmail.com>
2018-12-07 16:23:49 +0100

    Update configuration testing

--- nginx_pilot_api/tests/test_config.py
+++ nginx_pilot_api/tests/test_config.py
@@ -1,11 +1,9 @@
-import os
 import unittest
 
 from flask import current_app
 from flask_testing import TestCase
 
 from manage import app
-from nginx_pilot_api.config import basedir
 
 
 class TestDevelopmentConfig(TestCase):
@@ -16,7 +14,41 @@ class TestDevelopmentConfig(TestCase):
         return app
 
     def test_app_is_in_development_mode(self):
+        """ Checks that the development configuration is correctly loaded. """
+
         self.assertNotEqual(app.config.get('SECRET_KEY'), 'secret-to-change')
+        self.assertIsNotNone(current_app)
+        self.assertTrue(app.config.get('DEBUG'))
+
+
+class TestProductionConfig(TestCase):
+    """ List all functional tests for production environment. """
+
+    def create_app(self):
+        app.config.from_object('nginx_pilot_api.config.ProductionConfig')
+        return app
+
+    def test_app_is_in_production_mode(self):
+        """ Checks that the production configuration is correctly loaded. """
+
+        self.assertNotEqual(app.config.get('SECRET_KEY'), 'secret-to-change')
+        self.assertIsNotNone(current_app)
+        self.assertFalse(app.config.get('DEBUG'))
+
+
+class TestTestingConfig(TestCase):
+    """ List all functional tests for testing environment. """
+
+    def create_app(self):
+        app.config.from_object('nginx_pilot_api.config.TestingConfig')
+        return app
+
+    def test_app_is_in_testing_mode(self):
+        """ Checks that the testing configuration is correcty loaded. """
+
+        self.assertNotEqual('secret-to-change', app.config.get('SECRET_KEY'))
+        self.assertIsNotNone(current_app)
+        self.assertTrue(app.config.get('DEBUG'))
 
 
 if __name__ == '__main__':
