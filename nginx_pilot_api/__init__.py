5d7d8e61292c8945ca3c3c6ef5b7255cd2b5904a
Querdos <hamza.essayegh@protonmail.com>
2018-12-07 15:19:54 +0100

    Add first version of the create_app

---
+++ nginx_pilot_api/__init__.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+from flask import Flask
+
+from .config import config_by_name
+
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config_by_name.get(config_name, 'dev'))
+
+    return app
