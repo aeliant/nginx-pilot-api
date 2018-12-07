3358b58a1e6f74445faab15f9e8e0e7a5b8e6f82
Querdos <hamza.essayegh@protonmail.com>
2018-12-07 15:25:20 +0100

    Add first version of the manage.py file (application entry point)

---
+++ manage.py
@@ -0,0 +1,33 @@
+# coding=utf-8
+import os
+import unittest
+
+from flask_script import Manager
+from nginx_pilot_api import create_app
+
+app = create_app(os.getenv('FLASK_ENV', 'dev'))
+app.app_context().push()
+
+manager = Manager(app)
+
+
+@manager.command
+def run():
+    app.run()
+
+
+@manager.command
+def test():
+    """ Runs the unittest """
+    tests = unittest.TestLoader().discover(
+        'nginx_pilot_api/tests',
+        pattern='test*.py'
+    )
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccesful():
+        return 0
+    return 1
+
+
+if __name__ == "__main__":
+    manager.run()
