7804e1a34266378d15d54b69afaae6a16d0bac84
querdos <hamza.essayegh@protonmail.com>
2018-12-31 11:14:31 +0100

    Update manage.py (replace flask_script by flask command interface)

--- manage.py
+++ manage.py
@@ -1,8 +1,6 @@
 """Main project file, managing tests and server running."""
 import os
-import unittest
 
-from flask_script import Manager
 from nginx_pilot_api import create_app
 from nginx_pilot_api.controller import blueprint
 
@@ -10,27 +8,12 @@ app = create_app(os.getenv('FLASK_ENV') or 'dev')
 app.register_blueprint(blueprint)
 app.app_context().push()
 
-manager = Manager(app)
 
-
-@manager.command
+@app.cli.command()
 def run():
     """Run the server."""
     app.run()
 
 
-@manager.command
-def test():
-    """Runs the tests."""
-    tests = unittest.TestLoader().discover(
-        'nginx_pilot_api/tests',
-        pattern='test*.py'
-    )
-    result = unittest.TextTestRunner(verbosity=2).run(tests)
-    if result.wasSuccessful():
-        return 0
-    return 1
-
-
 if __name__ == "__main__":
-    manager.run()
+    run()
